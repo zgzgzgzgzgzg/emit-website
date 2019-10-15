752e7d98fd62cc6848aad8b075ee1e1333380a2e
Tourssliver <1341198325@qq.com>
2019-10-15 16:52:20 +0800

    ce shi fen zhi

---
+++ src/components/404.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        40444444444444444444444444444444444444(goutou.png)
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "404"
+    }
+</script>
+
+<style scoped>
+
+</style>
